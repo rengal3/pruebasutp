f3cc4545308c2c5919553b127670753f404f0dc4
gabrielpanta <gabrielpantajimenez@hotmail.com>
2025-05-05 19:00:28 -0500

    primer commit Gabriel

--- src/main/java/com/utp/utpproyectogit/UtpProyectoGit.java
+++ src/main/java/com/utp/utpproyectogit/UtpProyectoGit.java
@@ -20,7 +20,7 @@ public class UtpProyectoGit {
     
 
     public static int metodoNuevoRemoto(int b){
-        return 0;
+        return 1;
     }
     
     public static void main(String[] args) {
